d6d750290bfce48de92783fee620a26880ec625a
toqitahmid <rubaiathali@gmail.com>
2026-08-13 23:55:45 +0600

    learn about how to pass value as parameter in tsfunction

--- src/functions/function.ts
+++ src/functions/function.ts
@@ -1,24 +1,7 @@
-
-// let greetFun : (name:string) => string;
-// greetFun = function(name:string) : string{
-
-//     return `Hello ${name}`;
-// }
-// console.log(greetFun('Araf'));
-
-
-function logInfo (name:string, sem:string, id?:string):string{
-    return `${name} ${sem} ${id? id:''}`;
+function Display(a:number,b:number):number{
+    const sum = a+b;
+    return sum;
 }
-
-console.log(logInfo('Araf','5th','94'));
-console.log(logInfo('Sams','5th',));
-
-// default params 
-
-function display(name:string, age:number = 21):void{
-    console.log(name, age);
-}
-
-display('Toqi', 20);
-display('Toqi');
+console.log(typeof Display(2,4));
+console.log(Display(30,20));
+console.log(typeof Display(3,7).toFixed(2))
