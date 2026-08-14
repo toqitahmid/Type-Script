c2d34877dbc18e16be52aa6eb9fdaac7ecdc6f24
toqitahmid <rubaiathali@gmail.com>
2026-08-14 16:02:59 +0600

    learn how to pass values in ts functions with type sefty

---
+++ dist/functions/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet = (person) => {
+    return `Hello ${person}`;
+};
+const name = 'Abu Sams Araf';
+console.log(greet(name));
+//# sourceMappingURL=index.js.map
---
+++ src/functions/index.ts
@@ -0,0 +1,6 @@
+const greet = (person:string):string => {
+    return `Hello ${person}`;
+}
+
+const name:string = 'Abu Sams Araf';
+console.log(greet(name));
