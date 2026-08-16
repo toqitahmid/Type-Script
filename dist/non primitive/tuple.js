d22a72bcfff163dcdbd6ef4e694e9dd54f5c6e5a
toqitahmid <rubaiathali@gmail.com>
2026-08-17 00:30:25 +0600

    learn tuple in ts

---
+++ dist/non primitive/tuple.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const exam = [10, 20, 'Araf'];
+console.log(exam);
+//# sourceMappingURL=tuple.js.map
---
+++ src/non primitive/tuple.ts
@@ -0,0 +1,2 @@
+const exam:[number,number,string] = [10,20,'Araf'];
+console.log(exam);
